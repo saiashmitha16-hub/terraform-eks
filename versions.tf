07831510479c5a47b02b002f4e35748e483537ed
saiashmitha16-hub <saiashmitha16@gmail.com>
2025-09-29 20:08:13 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,18 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.3"
+
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     random = {
       source  = "hashicorp/random"
-      version = "~> 4.67"
+      version = "~> 3.5"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = ">= 2.7.1"
     }
     local = {
       source  = "hashicorp/local"
@@ -28,3 +28,8 @@ terraform {
     }
   }
 }
+
+
+  
+
+
