4bb92e79e697508bf455b67e1ba2ca60be472d25
saiashmitha16-hub <saiashmitha16@gmail.com>
2025-09-29 21:27:10 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,17 +1,16 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 22.0.0" 
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v22.0.0"
+
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
 
   tags = {
     cluster = "demo"
   }
-
-  vpc_id = module.vpc.vpc_id
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
@@ -20,7 +19,6 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-
     node_group = {
       min_size     = 2
       max_size     = 6
@@ -28,4 +26,3 @@ module "eks" {
     }
   }
 }
-
